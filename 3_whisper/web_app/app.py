d3445b98679968917bc8fb202c325eb453ba6ca3
Anand Singh <aanandsingh289@gmail.com>
2023-12-26 12:15:07 -0500

    added web app

---
+++ 3_whisper/web_app/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import openai
+import os
+os.environ['OPENAI_API_KEY']= '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'
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return render_template('index.html', message='No file part')
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return render_template('index.html', message='No selected file')
+
+    if file:
+        # Save the uploaded file to a desired location
+        file.save('uploads/' + file.filename)
+        audio_file = open('uploads/' + file.filename,'rb')
+        transcript = openai.Audio.transcribe("whisper-1",audio_file)
+        return render_template('index.html', message=transcript['text'], filename=file.filename)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
